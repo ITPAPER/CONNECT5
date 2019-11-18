d47f292d4fb5eec5c47146510279916e87a090c7
heegwon <skarnjs4097@gmail.com>
2019-11-18 11:28:04 +0900

    컨트롤러 info

--- src/main/java/study/spring/simplespring/controllers/HG_InfoController.java
+++ src/main/java/study/spring/simplespring/controllers/HG_InfoController.java
@@ -2,13 +2,21 @@ package study.spring.simplespring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HG_ManagerController {
+public class HG_InfoController {
 
+	@RequestMapping(value = "/_info/partner_HG.do", method = RequestMethod.GET)
+	public String partner(Model model) {
+		
+		
+
+		return "_info/partner_HG";
+
+	}
+	
 	@RequestMapping(value = "/_info/manager_HG.do", method = RequestMethod.GET)
 	public String manager(Model model) {
 		
