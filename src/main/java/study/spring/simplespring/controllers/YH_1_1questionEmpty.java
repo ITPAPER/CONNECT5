61c9023710d00a24717dee2f804a7caaf8e60a25
yuhan <dygks4308@gmail.com>
2019-12-03 09:14:20 +0900

    [YH] 1:1문의 수정

--- src/main/java/study/spring/simplespring/controllers/YH_1_1questionEmpty.java
+++ src/main/java/study/spring/simplespring/controllers/YH_1_1questionEmpty.java
@@ -1,34 +1,136 @@
 package study.spring.simplespring.controllers;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import study.spring.simplespring.helper.RegexHelper;
+import study.spring.simplespring.helper.WebHelper;
+import study.spring.simplespring.model.Board;
+import study.spring.simplespring.model.User;
+import study.spring.simplespring.service.BoardService;
+import study.spring.simplespring.service.UserService;
 
 @Controller
 public class YH_1_1questionEmpty {
 
-	@RequestMapping(value = "/_mypage/1_1questionEmpty_YH.do", method = RequestMethod.GET)
-	public String view(Model model) {
+	@Autowired
+	WebHelper webHelper;
 
-		return "_mypage/1_1questionEmpty_YH";
+	@Autowired
+	BoardService boardService;
 
-	}
-
-	@RequestMapping(value = "/_mypage/1_1questionWrite_YH.do", method = RequestMethod.GET)
-	public String wirte(Model model) {
-
-		return "_mypage/1_1questionWrite_YH";
-
-	}
-
-	@RequestMapping(value = "/_mypage/1_1question_replyWaiting_YH.do", method = RequestMethod.GET)
-	public String reply_wait(Model model) {
-
-		return "_mypage/1_1question_replyWaiting_YH";
-
-	}
+	@Autowired
+	RegexHelper regexHelper;
 	
+	@Autowired
+	UserService userService;
+	
+	
+
+	@Value("#{servletContext.contextPath}")
+	String contextPath;
+
+	@RequestMapping(value = "/_mypage/1_1questionEmpty_YH.do", method = RequestMethod.GET)
+	public ModelAndView view(Model model, HttpServletRequest request, HttpServletResponse response) {
+		User loginInfo = (User) webHelper.getSession("loginInfo");
+
+		if (loginInfo != null) {
+
+			String output = loginInfo.getUserName();
+
+			model.addAttribute("output", output);
+		}
+		
+		String UserName = loginInfo.getUserName();
+		int MemberId = loginInfo.getMemberId();
+		
+		String Title = webHelper.getString("Title");
+		String Content = webHelper.getString("Content");
+		String CreationDate = webHelper.getString("CreationDate");
+		int Category = webHelper.getInt("Category");
+		
+		Board input = new Board();
+		input.setUserName(UserName);
+		input.setTitle(Title);
+		input.setContent(Content);
+		input.setCreationDate(CreationDate);
+		input.setCategory(Category);
+		input.setMemberId(MemberId);
+		
+		List<Board> output2 = null;
+		try {
+
+			output2 = boardService.getBoardList(input);
+		} catch (Exception e) {
+			return webHelper.redirect(null, e.getLocalizedMessage());
+		}
+
+		// View에 추가
+		model.addAttribute("output2", output2);
+		return new ModelAndView("_mypage/1_1questionEmpty_YH");
+
+	}
+
+	@RequestMapping(value = "/_mypage/1_1questionadd.do", method = RequestMethod.GET)
+	public ModelAndView add(Model model) {
+		User loginInfo = (User) webHelper.getSession("loginInfo");
+
+		if (loginInfo != null) {
+
+			String output = loginInfo.getUserName();
+
+			model.addAttribute("output", output);
+		}
+		
+		int MemberId = loginInfo.getMemberId();
+		String Title = webHelper.getString("Title");
+		String Content = webHelper.getString("Content");
+		String CreationDate = webHelper.getString("CreationDate");
+		int Category = webHelper.getInt("Category");
+		String ContentImg = webHelper.getString("ContentImg");
+		
+		Board input = new Board();
+		input.setContent(Content);
+		input.setTitle(Title);
+		input.setCategory(Category);
+		input.setMemberId(MemberId);
+		input.setCreationDate(CreationDate);
+		input.setContentImg(ContentImg);
+		
+		
+		try {
+			// 데이터 저장
+			// --> 데이터 저장에 성공하면 파라미터로 전달하는 input 객체에 PK값이 저장된다.
+			boardService.addBoard(input);
+
+		} catch (Exception e) {
+			return webHelper.redirect(null, e.getLocalizedMessage());
+		}
+
+		// View에 추가
+
+		String redirectUrl = contextPath + "/home.do?BoardId=" + input.getBoardId();
+        return webHelper.redirect(redirectUrl, "저장되었습니다.");
+
+
+	}
+
+	@RequestMapping(value = "/_mypage/1_1questionadd_Ok.do", method = RequestMethod.GET)
+	public ModelAndView addOk(Model model) {
+	return null;
+
+	}
+
 	@RequestMapping(value = "/_mypage/1_1question_replyWait_YH.do", method = RequestMethod.GET)
 	public String wait(Model model) {
 
---
+++ src/main/webapp/WEB-INF/views/_mypage/1_1questionEmpty_YH.jsp
@@ -0,0 +1,114 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page trimDirectiveWhitespaces="true"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<!doctype html>
+<html>
+<head>
+<jsp:include page="../assets/inc/css.jsp" />
+<link rel="stylesheet" type="text/css"
+	href="${pageContext.request.contextPath}/assets/css/YH/Mypage/1_1questionEmpty_YH.css">
+
+<meta charset="utf-8" />
+<title>연-결</title>
+</head>
+<body>
+	<jsp:include page="../assets/inc/top.jsp" />
+
+
+
+	<!-- 변경사항  -->
+
+
+	<!-- 상단 이미지 ( 다른 이미지 저장 시 src 변경 이름 ) -->
+	<div class="boximg">
+		<img src="${pageContext.request.contextPath}/assets/img/question1.png"
+			alt="연-결" class="img-responsive" />
+	</div>
+	<!-- 상단 이미지 끝 -->
+
+	<div>
+		<div class="col-md-2">
+			<!-- 패널 타이틀1 -->
+			<div class="left sidebar1">
+				<h3 class="title">My 연-결</h3>
+				<!-- 사이드바 메뉴목록1 -->
+				<div class="list-group">
+					<a href="${pageContext.request.contextPath}/_mypage/myInfo_GD.do"
+						class="list-group-item">My현황</a> <a
+						href="${pageContext.request.contextPath}/_mypage/search_SE.do"
+						class="list-group-item">인연 찾기</a> <a
+						href="${pageContext.request.contextPath}/_mypage/1_1questionEmpty_YH.do"
+						class="list-group-item btncolor">1:1 문의</a> <a
+						href="${pageContext.request.contextPath}/_mypage/Ex-MatchingRecord_YB.do"
+						class="list-group-item">매칭 기록 보기 </a><a
+						href="${pageContext.request.contextPath}/_mypage/personal_information1_HG.do"
+						class="list-group-item">개인정보수정</a> <a
+						href="${pageContext.request.contextPath}/_mypage/withdrawal1_HG.do"
+						class="list-group-item">회원탈퇴</a>
+				</div>
+			</div>
+		</div>
+
+
+
+		<h3 class="col-md-10">1:1 문의</h3>
+		<div class="col-md-10 text1">
+			<h5>1:1 문의 상담</h5>
+		</div>
+
+		<div class="col-md-10 content">
+			<hr>
+			<div class="table-responsive">
+				<c:choose>
+					<%-- 조회결과가 없는 경우 --%>
+					<c:when test="${output2 == null || fn:length(output2) == 0}">
+						<tr>
+							<td colspan="9" align="center" id="question1">문의 내역이 없습니다.</td>
+						</tr>
+					</c:when>
+					<%-- 조회결과가 있는  경우 --%>
+					<c:otherwise>
+						<table>
+							<tr>
+								<th align="center" class="num">번호</th>
+								<th align="center">제목</th>
+								<th align="center" class="name">이름</th>
+								<th align="center" class="date">등록일</th>
+								<th align="center" class="reply_ok">답변여부</th>
+							</tr>
+						</table>
+
+
+						<%-- 조회 결과에 따른 반복 처리 --%>
+						<c:forEach var="item" items="${output2}" varStatus="status">
+							<c:out value="${status.count}" />
+						</c:forEach>
+						<c:forEach var="item" items="${output2}" varStatus="status">
+							<tr>
+								<th align="center">${item.getTitle()}</th>
+								<th align="center">${item.getContent()}</th>
+								<th align="center">${item.getUserName()}</th>
+								<th align="center">${item.getCreationDate()}</th>
+							</tr>
+						</c:forEach>
+					</c:otherwise>
+				</c:choose>
+			</div>
+			<hr>
+			<a
+				href="${pageContext.request.contextPath}/_mypage/1_1questionadd.do">
+				<button class="btn btn-default">문의하기</button>
+			</a>
+
+		</div>
+	</div>
+
+
+	<!-- 변경 사항 -->
+
+	<jsp:include page="../assets/inc/footer.jsp" />
+</body>
+</html>
